9023bcbdf3d04111019d7a38dad2dde8635b55f0
nguyen2598 <dinhnguyen250802@gmail.com>
2021-10-22 23:24:18 +0700

    hehe

--- main.js
+++ main.js
@@ -7,15 +7,15 @@
  * 5.set timeout
  * 6.set interval
  */
-alert('xin  chào ngiu')
+alert('xin  chào bae')
 var full;
-full='spacenema';
+full='mình lại làm ngiu nhé';
 alert(full);
 console.log('đây là dòng log')
-confirm('xác nhận là ngiu')
+confirm('xác nhận để làm ngiu')
 prompt('xác nhận tên ngiu')
 setTimeout(function() {
-alert('cảm ơn ngiu đã đợi')
+alert('cảm ơn đã làm ngiu')
 },1300)
 setInterval(function(){
     console.log('đây là lỗi ko phải chức năng'+String.random)
